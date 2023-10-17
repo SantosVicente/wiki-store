4364da5afedf205fcbefd72dd6713ef6690ffd57
SantosVicente <vidosantos45@gmail.com>
2023-10-16 23:01:31 -0300

    feat: add button, card and header to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Main Page</h1>
-      <p className="text-xl">This is the Main page</p>
-    </main>
-  );
+  return <main className="">Hello world</main>;
 }
