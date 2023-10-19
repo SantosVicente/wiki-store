9b831e65ee77eed56ca83825a95940c80802372a
SantosVicente <vidosantos45@gmail.com>
2023-10-19 19:59:32 -0300

    feat: moving section title to components/ui

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/products";
 import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailsPageProps {
   params: {
@@ -34,8 +35,9 @@ const ProductDetailsPage = async ({
     <div className="flex flex-col gap-5 pb-5">
       <ProductImages name={product.name} imageUrls={product.imageUrls} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+
       <div>
-        <h1 className="p-5 font-bold uppercase">Produtos Recomendados</h1>
+        <SectionTitle>Produtos Recomendados</SectionTitle>
         <ProductList
           products={product.category.Product.filter(
             (productItem) => productItem.id != product.id,
