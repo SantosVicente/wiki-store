b58812b17b4c3265fcff2dc940e6ac8ee57ed0cd
SantosVicente <vidosantos45@gmail.com>
2023-10-19 19:54:37 -0300

    feat: add recomend products

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/products";
+import ProductList from "@/components/ui/product-list";
 
 interface ProductDetailsPageProps {
   params: {
@@ -16,6 +17,13 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          Product: true,
+        },
+      },
+    },
   });
 
   if (!product) {
@@ -23,9 +31,17 @@ const ProductDetailsPage = async ({
   }
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 pb-5">
       <ProductImages name={product.name} imageUrls={product.imageUrls} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+      <div>
+        <h1 className="p-5 font-bold uppercase">Produtos Recomendados</h1>
+        <ProductList
+          products={product.category.Product.filter(
+            (productItem) => productItem.id != product.id,
+          )}
+        />
+      </div>
     </div>
   );
 };
