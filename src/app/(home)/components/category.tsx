52372dac77fffbfba520905f79b7e865c5a37a63
SantosVicente <vidosantos45@gmail.com>
2023-10-18 23:14:28 -0300

    feat: link route creation in category buttons on home page

--- src/app/(home)/components/category.tsx
+++ src/app/(home)/components/category.tsx
@@ -3,6 +3,7 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryProps {
   category: Category;
@@ -10,13 +11,15 @@ interface CategoryProps {
 
 const Category = ({ category }: CategoryProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
