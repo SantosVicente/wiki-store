20aaf8dca7cdb75f6702f0c872ba9c213470b77d
SantosVicente <vidosantos45@gmail.com>
2023-10-20 19:35:37 -0300

    feat: added "Finalizar Compra" Button to Cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,11 +1,13 @@
-import { ShoppingCartIcon } from "lucide-react";
+import { CircleDollarSign, ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { SheetHeader } from "./sheet";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/products";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
@@ -22,49 +24,56 @@ const Cart = () => {
         </Badge>
       </SheetHeader>
 
-      <div className="flex flex-col gap-5">
-        {products.length === 0 ? (
-          <p className="text-center text-sm text-gray-400">
-            Seu carrinho está vazio
-          </p>
-        ) : (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductTotalPrice(product as any) as any}
-            />
-          ))
-        )}
-      </div>
+      <ScrollArea className="h-full">
+        <div className="flex h-full flex-col gap-5">
+          {products.length === 0 ? (
+            <p className="text-center text-sm text-gray-400">
+              Seu carrinho está vazio
+            </p>
+          ) : (
+            products.map((product) => (
+              <CartItem
+                key={product.id}
+                product={computeProductTotalPrice(product as any) as any}
+              />
+            ))
+          )}
+        </div>
+      </ScrollArea>
       {products.length > 0 && (
         <div className="flex flex-col gap-3">
-          <div className="h-[1px] w-full bg-zinc-800" />
+          <Separator />
 
           <div className="flex items-center justify-between text-sm opacity-60">
             <p>Subtotal</p>
             <p className="line-through">R$ {subtotal.toFixed(2)}</p>
           </div>
 
-          <div className="h-[1px] w-full bg-zinc-800" />
+          <Separator />
 
           <div className="flex items-center justify-between text-sm">
             <p>Entrega</p>
             <p className="uppercase">Grátis</p>
           </div>
 
-          <div className="h-[1px] w-full bg-zinc-800" />
+          <Separator />
 
           <div className="flex items-center justify-between text-sm">
             <p>Desconto</p>
             <p>- R$ {totalDiscount.toFixed(2)}</p>
           </div>
 
-          <div className="h-[1px] w-full bg-zinc-800" />
+          <Separator />
 
           <div className="flex items-center justify-between text-base font-bold">
             <p>Total a Pagar</p>
             <p>R$ {total.toFixed(2)}</p>
           </div>
+
+          <Button className="h-12 gap-1 rounded-xl uppercase">
+            <CircleDollarSign size={16} />
+            <p className="font-bold">Finalizar Compra</p>
+          </Button>
         </div>
       )}
     </>
