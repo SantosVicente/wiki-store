af350a2baa87a006ca9263eee4d42cc06421cd36
SantosVicente <vidosantos45@gmail.com>
2023-10-23 21:00:38 -0300

    feat: add createOrder server action

---
+++ src/actions/order.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { prismaClient } from "@/lib/prisma";
+import { CartProduct } from "@/providers/cart";
+
+export const createOrder = async (
+  cartProducts: CartProduct[],
+  userId: string,
+) => {
+  const order = await prismaClient.order.create({
+    data: {
+      userId: userId,
+      status: "WAITING_FOR_PAYMENT",
+      orderProducts: {
+        createMany: {
+          data: cartProducts.map((cartProduct) => ({
+            basePrice: cartProduct.basePrice,
+            quantity: cartProduct.quantity,
+            discountPercentage: cartProduct.discountPercentage,
+            productId: cartProduct.id,
+          })),
+        },
+      },
+    },
+  });
+
+  return order;
+};
